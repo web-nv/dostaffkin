62ddd45a5975f293a525d392c141856ff6793d62
web-nv <156799082+web-nv@users.noreply.github.com>
2026-02-17 12:57:37 +0000

    Optimization

--- src/app/pages/order/order.ts
+++ src/app/pages/order/order.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from '../../header/header';
 import { DELIVERY_SIZES, DELIVERY_SPEEDS } from './order.config';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UpperCasePipe } from '@angular/common';
 import { DeliveryApi } from '../../services/delivery-api';
+import { ToastrService } from 'ngx-toastr';
 
 declare var ymaps: any;
 
@@ -16,6 +17,8 @@ declare var ymaps: any;
 export class Order {
   public readonly sizes = DELIVERY_SIZES;
   public readonly speeds = DELIVERY_SPEEDS;
+
+  toastr = inject(ToastrService);
 
   public map: any;
   private mapRoute: any;
@@ -42,16 +45,40 @@ export class Order {
 
   ngOnInit() {
     ymaps.ready(() => {
-      this.map = new ymaps.Map('map', {
-        center: [55.751244, 37.618423],
-        zoom: 5,
-        controls: ['zoomControl']
-      });
-
-      // Подключаем подсказки адресов к полям от яндекса
-      (new ymaps.SuggestView('from')).events.add('select', (event: any) => (this.routeForm.controls['from'].setValue(event.get('item')?.value ?? '')));
-      (new ymaps.SuggestView('to')).events.add('select', (event: any) => (this.routeForm.controls['to'].setValue(event.get('item')?.value ?? '')));
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          (pos) => (this.init(pos.coords.latitude, pos.coords.longitude)),
+          () => this.init());
+      } else {
+        this.init();
+      }
     });
+  }
+
+  public init(lat: any = null, lon: any = null) {
+    this.map = new ymaps.Map('map', {
+      center: [lat ?? 55.751244, lon ?? 37.618423],
+      zoom: lat && lon ? 15 : 5,
+      controls: ['zoomControl']
+    });
+
+    // Обратное геокодирование: определяем ближайший адрес по координатам, подставляем в Откуда и добавляем поинт на карту
+    if (lat != null && lon != null) {
+      ymaps.geocode([lat, lon], { kind: 'house' }).then(
+        (res: any) => {
+          const first = res.geoObjects.get(0);
+          if (first?.getAddressLine) {
+            this.routeForm.controls['from'].setValue(first.getAddressLine());
+            this.map.geoObjects.add(first);
+          }
+        },
+        () => { }
+      );
+    }
+
+    // Подключаем подсказки адресов к полям от яндекса
+    (new ymaps.SuggestView('from')).events.add('select', (event: any) => (this.routeForm.controls['from'].setValue(event.get('item')?.value ?? '')));
+    (new ymaps.SuggestView('to')).events.add('select', (event: any) => (this.routeForm.controls['to'].setValue(event.get('item')?.value ?? '')));
   }
 
   public selectSize(size: string) {
@@ -123,18 +150,18 @@ export class Order {
 
   private failedCalculation() {
     this.calculationResult.set(null);
-    alert('Не удалось построить маршрут. Проверьте адреса и выбранные параметры.');
+    this.toastr.error('Не удалось построить маршрут. Проверьте адреса и выбранные параметры.');
   }
 
   public submitOrder() {
     const calculation = this.calculationResult();
     if (!calculation) {
-      alert('Сначала рассчитайте стоимость, чтобы оформить заявку');
+      this.toastr.error('Сначала рассчитайте стоимость, чтобы оформить заявку');
       return;
     }
 
     if (this.orderForm.invalid) {
-      alert('Введите имя и корректный телефон');
+      this.toastr.error('Введите имя и корректный телефон');
       return;
     }
 
@@ -154,10 +181,11 @@ export class Order {
 
     this.deliveryApi.createDelivery(payload).subscribe((response) => {
       if ('error' in response) {
-        alert(response.error);
+        this.toastr.error(response.error);
         return;
       }
 
+      this.toastr.success('Заявка успешно оформлена!');
       this.orderId.set(response.id);
     });
 
